a8427e2ce4aa7367eff5c1ab1b9020afe0bb51b7
TungIT <tungdrivehaui01@gmail.com>
2024-06-24 15:06:12 +0700

    Theme dark/night p2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import AccessAlarm from '@mui/icons-material/AccessAlarm'
-import Home from '@mui/icons-material/Home'
 import Button from '@mui/material/Button'
+import AccessAlarm from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Home from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
-import {
-  useColorScheme
-} from '@mui/material/styles'
 
+import { useColorScheme } from '@mui/material/styles'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import { Box } from '@mui/material'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = event => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
   return (
@@ -23,9 +66,11 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <hr />
-      <div>Tùng HAHA</div>
+      <div>trelloVip</div>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-import {experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 // Create a theme instance.
 const theme = extendTheme({
@@ -8,12 +8,14 @@ const theme = extendTheme({
         primary: teal,
         secondary: deepOrange
       }
+      // spacing: factor => `${0.25 * factor}rem`
     },
     dark: {
       palette: {
         primary: cyan,
         secondary: orange
       }
+      // spacing: factor => `${0.25 * factor}rem`
     }
   }
   // ...other properties
